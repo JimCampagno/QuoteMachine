154cc1c9d3a8e9117fb652d74d069fe5abc37413
JimCampagno <jimcampagno@gmail.com>
2015-04-11 21:46:10 -0400

    Magic happening

---
+++ QuoteMachine/DisplayPeopleCell.h
@@ -0,0 +1,19 @@
+//
+//  DisplayPeopleCell.h
+//  QuoteMachine
+//
+//  Created by Jim Campagno on 4/11/15.
+//  Copyright (c) 2015 Jim Campagno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DisplayPeopleCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UIButton *firstButton;
+@property (weak, nonatomic) IBOutlet UIButton *secondButton;
+@property (weak, nonatomic) IBOutlet UIButton *thirdButton;
+@property (weak, nonatomic) IBOutlet UIButton *fourthButton;
+
+
+@end
---
+++ QuoteMachine/SelectScreenViewController.h
@@ -0,0 +1,13 @@
+//
+//  SelectScreenViewController.h
+//  QuoteMachine
+//
+//  Created by Jim Campagno on 4/11/15.
+//  Copyright (c) 2015 Jim Campagno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SelectScreenViewController : UIViewController
+
+@end
